ed90e512739708fe52c1a4543922727c9c79bf04
Sotatek-William <william.tran@sotatek.com>
2024-06-20 16:14:05 +0700

    fix: catch error

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,29 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { TonConnectUIProvider } from "@tonconnect/ui-react";
 import { AppProvider } from "./contexts/AppContext";
 import Home from "./layout/home";
 
 const App: React.FC = () => {
+  useEffect(() => {
+    try {
+      if (typeof (window as any).TelegramGameProxy !== "undefined") {
+        (window as any).TelegramGameProxy.receiveEvent(
+          "event_name",
+          "event_data"
+        );
+      } else {
+        console.error("TelegramGameProxy is not defined");
+      }
+    } catch (error) {
+      console.error("Caught error:", error);
+    }
+  }, []);
+
   return (
     <TonConnectUIProvider manifestUrl="https://ton-demo.sotatek.works/tonconnect-manifest.json">
       <AppProvider>
         <div className="App">
-            <Home />
+          <Home />
         </div>
       </AppProvider>
     </TonConnectUIProvider>
